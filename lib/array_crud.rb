5ab9268721595c4a229f3c258dbc77531961e500
Vasilios Kambouras <vasilioskw@gmail.com>
2020-08-20 18:48:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array = [arraya] << "!"
+  add_element_to_end_of_array = [arrays] << "!"
     return [1, 2, 3]
 end
 
